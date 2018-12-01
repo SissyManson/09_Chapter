0fc9cf777ac8284d7507ae20f5ae56f42f150550
Sissy Dancheva <komandira123@gmail.com>
2018-12-01 05:22:03 +0200

    Add project files.

---
+++ 07_Task/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _07_Task
+{
+    class Program
+    {
+        public static string Reverse(string number)
+        {
+            char[] charArray = number.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            string number = Console.ReadLine();
+
+            Console.WriteLine(Reverse(number));
+        }
+    }
+}
